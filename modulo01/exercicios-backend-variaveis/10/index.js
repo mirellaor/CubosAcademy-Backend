ea5ba2eba6d923b5748c2837d929285d85a90828
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:35:01 -0300

    feat: Calculando delta

---
+++ modulo01/exercicios-backend-variaveis/10/index.js
@@ -0,0 +1,9 @@
+//Calculando delta
+
+let coeficienteA = 5;
+let coeficienteB = 5;
+let coeficienteC = 1;
+
+let delta = ((coeficienteB ** 2) - (4 * coeficienteA * coeficienteC));
+
+console.log(`O valor de delta é: ${delta}`);
