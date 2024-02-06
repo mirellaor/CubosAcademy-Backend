be4f09d9f3babae12beb69e8652c2063aa5eea68
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:34:43 -0300

    feat: Soma dos ângulos internos de um polígono

---
+++ modulo01/exercicios-backend-variaveis/09/index.js
@@ -0,0 +1,8 @@
+//Soma dos ângulos internos de um polígono
+
+let qntdLadosPoligono = 6;
+let SomaAngulosInternos = ((qntdLadosPoligono - 2) * 180);
+let ValorAngulo = (SomaAngulosInternos / qntdLadosPoligono);
+
+console.log(`A soma dos ângulos internos é: ${SomaAngulosInternos}`);
+console.log(`A soma de cada ângulos é: ${ValorAngulo}`);
