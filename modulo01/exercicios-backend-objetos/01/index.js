aab99af62394523c182d61a9e80851982cc014de
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:40:35 -0300

    feat: Cadastro de usuário

---
+++ modulo01/exercicios-backend-objetos/01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "rua Leonidio",
+    numero: 17,
+    complemento: "casa",
+    bairro: "centro",
+    cep: 42335000,
+    cidade: "Cruz",
+    estado: "Ba",
+    pais: "Brasil"
+};
+
+const usuario = {
+    nome: "Mirella",
+    email: "mirellaoliveeira@hotmail.com",
+    telefone: 99107,
+    data_nascimento: "14/02",
+    endereco
+}
+
+console.log(usuario)
