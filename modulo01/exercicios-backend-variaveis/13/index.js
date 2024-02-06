0c69f77232b0bf467c52b685b0dc4353bb868bbf
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:36:01 -0300

    feat: Área total de um cilindro

---
+++ modulo01/exercicios-backend-variaveis/13/index.js
@@ -0,0 +1,8 @@
+//Área total de um cilindro.
+
+let raioDaBase = 3;
+const pi = 3.14;
+let alturaDoCilindro = 1;
+
+let AreaTotalDoCilindro = ((2 * pi * raioDaBase) * (raioDaBase + alturaDoCilindro));
+console.log(`A área total do cilindro é: ${AreaTotalDoCilindro}`);
