402244c61048f2a5f0f794e18f4926eeceba401b
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:40:53 -0300

    feat: Lista de veículos

---
+++ modulo01/exercicios-backend-objetos/02/index.js
@@ -0,0 +1,27 @@
+const carros = [{
+    marca: "toyota",
+    modelo: "hilux",
+    ano: 2023,
+    cor: "branco",
+    quantidade_portas: "4",
+    automatico: true
+},
+{
+    marca: "Volkswagen",
+    modelo: "Gol",
+    ano: 2022,
+    cor: "vermelho",
+    quantidade_portas: 4,
+    automatico: false
+},
+{
+    marca: "Toyota",
+    modelo: "corolla",
+    ano: 2022,
+    cor: "prata",
+    quantidade_portas: 4,
+    automatico: true
+}
+];
+
+console.log(carros);
