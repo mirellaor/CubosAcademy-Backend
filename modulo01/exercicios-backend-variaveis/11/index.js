9cd2d46e724491857ee484bb813abb1f8996669c
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:35:18 -0300

    feat: calculo do volume de uma esfera

---
+++ modulo01/exercicios-backend-variaveis/11/index.js
@@ -0,0 +1,7 @@
+//calculo do volume de uma esfera
+
+const pi = 3.14;
+let diametro = 6;
+
+let volumeDaEsfera = ((4 / 3) * pi * ((diametro / 2) ** 3));
+console.log(`O volume de uma esfera de raio ${diametro / 2} é ${volumeDaEsfera}`);
