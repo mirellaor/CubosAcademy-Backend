10cff4652f5134f59d05679dc78f7238ceaf4798
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:32:24 -0300

    feat: calculando  desconto

---
+++ modulo01/exercicios-backend-variaveis/03/index.js
@@ -0,0 +1,7 @@
+//calculando  desconto
+
+precoTenis = 129.99;
+meuDinheiro = 80.00;
+descontoDinheiro = precoTenis - meuDinheiro;
+descontoPorcentagem = ((descontoDinheiro * 100) / precoTenis);
+console.log(`O desconto em porcentagem é: ${descontoPorcentagem}`);
