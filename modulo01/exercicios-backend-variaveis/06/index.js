cfb3a0f6a787921305356a7be6f554f2df64137a
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:33:36 -0300

    feat: Calculando a velocidade média

---
+++ modulo01/exercicios-backend-variaveis/06/index.js
@@ -0,0 +1,9 @@
+//Calculando a velocidade média
+
+distPercorrida = 500;
+tempo = 10;
+
+velocidadeM = (distPercorrida / tempo);
+velocidadeKm = (velocidadeM * 3.6);
+
+console.log(`A velocidade em Km/h é ${velocidadeKm}`);
