51d91b7664e718467a74f8aff5dea67f395ebfca
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-06 10:34:21 -0300

    feat: Calculando do perímetro e área de um círculo

---
+++ modulo01/exercicios-backend-variaveis/08/index.js
@@ -0,0 +1,9 @@
+//Calculo do perímetro e área de um círculo
+
+let raioCirculo = 1
+const valorPi = 3.14;
+calculoComprimento = (2 * valorPi * raioCirculo);
+calculoArea = (valorPi * (raioCirculo ** 2));
+
+console.log(`O comprimento do circulo é ${calculoComprimento}`);
+console.log(`A área do circulo é ${calculoArea}`);
