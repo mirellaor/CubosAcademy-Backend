419d4f7bcf4b48477c9262ec4fe2f4f1df970562
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-28 10:17:58 -0300

    feat: exercicio nomes com a letra a

---
+++ modulo01/exercicios-backend-arrays-loops/07/index.js
@@ -0,0 +1,13 @@
+const nomes = ["Ana", "Joana", "aline", "amanda"];
+let nomesComA = [];
+for (let nomeA of nomes) {
+    if (nomeA[0] == "a" || nomeA[0] == "A") {
+        nomesComA.push(nomeA);
+    }
+}
+console.log(nomesComA);
+
+
+
+// nomesComA.push(nomeA);
+//     for (let nomesAA of nomesComA){
