8d2aba70a5579fd9643ad4bd004ecd08fc88d95e
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-28 10:29:32 -0300

    feat: exercicio par ou impar

---
+++ modulo01/exercicios-backend-condicionais/02/index.js
@@ -0,0 +1,12 @@
+//par ou ímpar
+
+const jogada1 = 1;
+const jogada2 = 3;
+
+const somaJogadas = jogada1 + jogada2;
+
+if (somaJogadas % 2 == 0) {
+    console.log("par")
+} else {
+    console.log("ímpar")
+}
