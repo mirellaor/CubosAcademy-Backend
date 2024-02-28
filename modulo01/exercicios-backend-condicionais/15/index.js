e212a74c400a2a8f942fd224b6354eff2e22282a
mirellaor <mirellaoliveeira@hotmail.com>
2024-02-28 10:33:00 -0300

    feat: exercicio nome para exibição

---
+++ modulo01/exercicios-backend-condicionais/15/index.js
@@ -0,0 +1,11 @@
+const primeiroNome = "Mario";
+const sobrenome = "";
+const apelido = "Bros";
+
+if (apelido) {
+    console.log(apelido);
+} else if (sobrenome) {
+    console.log(primeiroNome + " " + sobrenome);
+} else {
+    console.log(primeiroNome);
+}
